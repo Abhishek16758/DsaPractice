fa60068af96b6a5ed7310d4d57edee25db63cce5
Abhishek16758 <A8728517@gmail.com>
2026-04-09 17:44:06 +0530

    Create the Map (Key is a String, Value is an Integer)

--- Practice Day1/Main.java
+++ Practice Day1/Main.java
@@ -1,1 +1,14 @@
+package com.practice;
+import java.util.HashMap;
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 HashMap <String, Integer> studentGrades = new HashMap<>();
+		 studentGrades.put("Alice", 95);
+		 studentGrades.put("Bob", 82);      
+		 System.out.println(studentGrades.get("Alice"));
+	}
+
+}
 
